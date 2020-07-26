normalisation added and binary_crossentropy loss fixed

loss/binary_crossentropy.py
@@ -4,4 +4,4 @@ import numpy as np
 class binary_crossentropy:
 
     def calculate(p,y):
-        return sum((y*np.log(p)) + ((1-y)*np.log(1-p)))
+        return sum((y*np.log(p)) + ((1-y)*np.log(1-p)))/p.shape[0]

model/Linear.py
@@ -51,7 +51,7 @@ class Linear:
         input_batch=np.array_split(input_batch,number_of_batchs)
         label_batch=np.array_split(label_batch,number_of_batchs)
         for (i,j) in enumerate(zip(input_batch,label_batch)):
-            pred_label_batch = np.array([self.eval(k) for k in j[0]])/40000000000
+            pred_label_batch = np.array([self.eval(k) for k in j[0]])
             print(pred_label_batch)
             ground_label_batch = j[1]
             print("EPOCH:: 1  BATCH:: ", i, "\n","loss:: ",self.loss.calculate(pred_label_batch,ground_label_batch))

test.py
@@ -17,12 +17,12 @@ model.add(MaxPooling2D((2,2)))
 model.add(Flatten())
 model.add(Dense(128))
 model.add(Dense(64))
-model.add(Dense(1))
+model.add(Dense(1,activation=Sigmoid))
 model.summary()
 
 model.eval(image)
 input_data = open("input_data.pickle","rb")
-input_data = np.array(pickle.load(input_data))
+input_data = np.array(pickle.load(input_data))/255.0
 
 label = open("label.pickle","rb")
 label = np.expand_dims(np.array(pickle.load(label)),axis=1)