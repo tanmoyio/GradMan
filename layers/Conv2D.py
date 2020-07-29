backprop corrected

layers/Conv2D.py
@@ -18,10 +18,10 @@ class Conv2D:
 
 
     def init_weights(self):
-        self.weights = np.random.uniform(low=-5.0, high=5.0, size=(self.units, self.kernel_size[0], self.kernel_size[1]))
+        self.weights = np.random.uniform(low=-1.0, high=1.0, size=(self.units, self.kernel_size[0], self.kernel_size[1]))
 
     def normalize(self,input_array):
-        return (input_array/(np.amax(input_array)+np.amin(input_array)))*5.0
+        return (input_array/(np.amax(input_array)+np.amin(input_array)))
 
     
     def compute_grad(self,input_array):

layers/Dense.py
@@ -23,7 +23,7 @@ class Dense:
 
 
     def normalize(self,input_array):
-        return (input_array/(np.amax(input_array)+np.amin(input_array)))*5.0
+        return (input_array/(np.amax(input_array)+np.amin(input_array)))*4.8
 
     
     def compute_grad(self,input_array):
@@ -36,6 +36,8 @@ class Dense:
         self.result = np.squeeze(np.dot(x1,x2),axis=0)
         if self.normalize_signal == True:
             self.result = self.normalize(self.result)
+        if self.normalize_signal == False:
+            self.result = self.result/(self.output_units*np.amax(self.input_array)*np.amax(self.result))
         if self.activation != None:
             self.result = self.activation.calculate(self.result)
         self.output_shape = self.result.shape

model/Linear.py
@@ -53,7 +53,7 @@ class Linear:
         for (i,j) in enumerate(zip(input_batch,label_batch)):
             pred_label_batch = np.array([self.eval(k) for k in j[0]])
             ground_label_batch = j[1]
-            print("loss:: ",self.loss.calculate(pred_label_batch,ground_label_batch))
+            print("loss:: ",self.loss.calculate(pred_label_batch,ground_label_batch)[-1])
             loss_grad = self.loss.compute_grad(pred_label_batch,ground_label_batch)
             counter = 0
             for layer in self.graph[::-1]: