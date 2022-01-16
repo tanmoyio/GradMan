7e10c6eb613297f48bf0433c2145af59e0627361
tanmoyio <tanmoyf2@gmail.com>
2022-01-16 22:20:26 +0530

    nn.Module added

--- demo.py
+++ demo.py
@@ -1,12 +1,30 @@
+import gradman.nn as nn
 from gradman import Tensor
-import numpy as np
+from gradman.optim import GDE
 
-'''
-a = Tensor([1.0, 0.5, 0.8], requires_grad=True)
-b = a.sum()
-b.backward()
-print(a.grad)
-'''
 
-a = Tensor(np.random.randn(10,10))
-print(a[:5, :2])
+class DemoModel(nn.Module):
+    def __init__(self):
+        super(DemoModel, self).__init__()
+        self.l1 = nn.Linear(6,3)
+        self.l2 = nn.Linear(3,1)
+
+    def forward(self, i):
+        return self.l2(self.l1(i))
+
+class D2(nn.Module):
+    def __init__(self):
+        super(D2, self).__init__()
+        self.model = DemoModel()
+        
+    def forward(self,i):
+        return self.model(i)
+
+model = D2()
+optim = GDE()
+for _ in range(10):
+    y = model(Tensor([[1,2,3,3,4,6]]))
+    y = (y - Tensor([2])).sum()
+    print(y)
+    y.backward()
+    optim.step(model.parameters())
---
+++ gradman/nn.py
@@ -0,0 +1,34 @@
+from gradman import Tensor
+import numpy as np
+
+class Module:
+
+    def _forward(self,i):
+        return self.forward(i)
+
+    __call__ = _forward
+
+    def parameters(self):
+        return [getattr(self, i) for i in self.__dict__]
+
+    
+class Linear(Module):
+    def __init__(self, idim:int, odim:int):
+        super(Linear, self).__init__()
+
+        self.idim = idim
+        self.odim = odim
+
+        self.w = Tensor(np.random.randn(idim, odim), requires_grad=True)
+        self.b = Tensor(np.random.randn(), requires_grad=True)
+
+    def forward(self, i: Tensor):
+        return (i @ self.w) + self.b
+
+    __call__ = forward
+
+    def parameters(self):
+        return [self.w, self.b]
+
+    def update(self, p_new):
+        self.w, self.b = p_new
